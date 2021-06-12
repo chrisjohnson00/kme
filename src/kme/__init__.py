16fcc354bc96710f87e4c7b9fe02e056d86c6080
Chris Johnson <chrisjohnson00@hotmail.com>
2021-06-11 17:49:09 -0700

    fix: decoding KMEMessage fails without completion_topic in incoming message

--- src/kme/__init__.py
+++ src/kme/__init__.py
@@ -1,4 +1,13 @@
 from kme.kme import KME, KMEMessage
+import logging
+
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+
+logging.getLogger(__name__).addHandler(NullHandler())
 
 __all__ = [
     'KME',
